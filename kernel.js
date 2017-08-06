73baab70dbfddf9eb46598ab415ec7e8eb451f6d
Marfjeh <marfie1997@ziggo.nl>
2017-08-07 00:42:28 +0200

    small changes. ugh i really hate setInterval though..
    Signed-off-by: Marfjeh <noreply@marfprojects.nl>

--- kernel.js
+++ kernel.js
@@ -1,5 +1,5 @@
 const Rcon = require('modern-rcon');
-var host = "127.0.0.1";
+var host = "192.168.178.29";
 var rconpassword = "lel";
 var rconport = 25575;
 
@@ -12,7 +12,7 @@ function rconsend(command) {
     rconserver.connect().then(() => {
         return rconserver.send(command);
     }).then(res => {
-        console.log(res);
+        //console.log(res);
     }).then(() => {
         return rconserver.disconnect();
     })
@@ -22,18 +22,24 @@ function sendtitle(player, title, color = "white") {
     rconsend('title ' + player +' title ["",{"text":"' + title + '","' + color + '":"white","bold":true}]');
 }
 
+function playsound(sound, player, volume, pitch = "1") {
+    rconsend("playsound " + sound + " voice " + player + " ~ ~ ~ " + volume + " " + pitch);
+}
+
 function StartGame() {
     zombiespawnpoints = ["-1660 5 228", "-1640 5 236", "-1634 5 260"];
     wave ++;
     maxzombies = 10;
+    playsound("wavestart", "@a", "999999")
     zombiesspawned = 0;
     sendtitle("@a", "Wave: " + wave, "red");
-    var zombiesspaner = setInterval(function() { //ZombieSpawning
+    var zombietimer = setInterval(function() { //ZombieSpawning
             rconsend("summon minecraft:zombie " + zombiespawnpoints[Math.floor(Math.random() * zombiespawnpoints.length)]);
             zombiesspawned ++;
-            if (zombiesspawned >= maxzombies) {
-                nextwave();
-                clearInterval(zombiesspaner)
+            console.log("spawned: " + zombiesspawned + " max: " + maxzombies)
+            if(zombiesspawned >= 10) {
+                clearInterval(zombietimer);
+                testforZombies();
             }
     }, 1000);
 }
@@ -45,11 +51,14 @@ function nextwave() {
 }
 
 function testforZombies() {
-    
+    var testforzombie = setInterval(function() {
+        testf = rconsend("testfor @e[type=Zombie]");
+        console.log(testf);
+    }, 500 );
 }
 
 console.log("GameCore Ready.");
-console.warn("[!] Default IP: 127.0.0.1, Port: 25575, Password: lel")
+console.warn("[!] Default IP: " + host + ", Port: " + rconport + ", Password: " +  rconpassword)
 
 const stdin = process.openStdin();
 stdin.addListener("data", function(d) {
@@ -68,7 +77,11 @@ stdin.addListener("data", function(d) {
         rconport = parseInt(readline.substring(5));
     }
     if (readline === "start") {
+        rconsend("kill @e[type=!player]");
         StartGame();
+    }
+    if (readline === "nxtwave") {
+        nextwave();
     }
     if (readline === "killz") {
        rconsend("kill @e[type=!player]");
